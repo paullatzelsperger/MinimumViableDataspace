b6da3fdac7aa0d9d8db3fcddc441da3ae397fb3b
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2025-05-13 10:18:15 +0200

    added AAS synchronizer

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,6 +29,8 @@ include(":extensions:did-example-resolver")
 include(":extensions:catalog-node-resolver")
 include(":extensions:dcp-impl")
 include(":extensions:superuser-seed")
+include(":extensions:aas-dataplane")
+include(":extensions:aas-synchronizer")
 //include(":tests:performance")
 include(":tests:end2end")
 
